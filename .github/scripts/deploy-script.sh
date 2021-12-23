8a5cbf50540c001f7868dcd7a5d36cc56febd0a0
Jafar Aziz <aziz@jafaraziz.com>
2021-12-24 02:07:10 +0700

    Multiline string on github workflows logs

--- .github/scripts/deploy-script.sh
+++ .github/scripts/deploy-script.sh
@@ -6,11 +6,11 @@ DEPLOY_KEY=${SERVER_AUTHORIZATION?Need a value}
 DEPLOY_URL=${SERVER_URL?Need a value}
 
 # Trigger Deployment Scripts On server
-response=($(curl -X POST -d "token=${GITHUB_TOKEN}&type=${1}" -H "Authorization:${DEPLOY_KEY}" -w "\n%{http_code}" ${DEPLOY_URL}))
-body=${response[@]::${#response[@]}-1}
-code=${response[-1]}
+response=$(curl -X POST -d "token=${GITHUB_TOKEN}&type=${1}" -H "Authorization:${DEPLOY_KEY}" -w "\n%{http_code}" ${DEPLOY_URL})
+code=$(tail -n1 <<<"$response")
+body=$(sed '$ d' <<<"$response")
 
-echo $body
+echo "$body"
 
 if [ $code -ne 200 ]; then
     exit 20
