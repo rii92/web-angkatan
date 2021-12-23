6cf157946c5298f2dee9f42c230aa93de90b8d2c
Jafar Aziz <44570698+JfrAziz@users.noreply.github.com>
2021-12-23 17:20:01 +0700

    GitHub workflows (#19)
    * Add Github Workflows

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Symfony\Component\Process\Process;
 
 class DeploymentController extends Controller
@@ -12,21 +13,29 @@ class DeploymentController extends Controller
      *
      * @return void
      */
-    public function run(Request $request){
+    public function run(Request $request)
+    {
         $output = '';
 
         try {
-            $process = new Process([base_path() . "/deploy.sh"]);
+            $request->validate([
+                'type' => [
+                    'required',
+                    Rule::in(['pull-repository', 'backup', 'migration', 'clear-cache']),
+                ],
+            ]);
+
+            $process = new Process([base_path() . "/deploy.sh", $request->input('type'), $request->input('token')]);
 
             $process->run(function ($type, $line) use (&$output) {
                 $output .= $line;
             });
-    
+
             if ($process->getExitCode()) {
                 $output .= $process->getExitCode();
                 throw new \Exception($output);
             }
-    
+
             return response($output, 200);
         } catch (\Exception $e) {
             return response($e->getMessage(), 400);
