7338e5dfe5ecdf9eefdc89b20bae08126400ee22
Jafar Aziz <aziz@jafaraziz.com>
2022-01-02 22:15:13 +0700

    nvm, just update missing asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <x-jet-banner />
 
     <div class="min-h-screen bg-gray-100">
-        <nav class="bg-white border-b border-gray-500 shadow-sm">
+        <nav class="bg-white border-b shadow-sm">
             <!-- Primary Navigation Menu -->
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
--- resources/views/pagination/main.blade.php
+++ resources/views/pagination/main.blade.php
@@ -102,7 +102,7 @@
                                         @if ($page == $paginator->currentPage())
                                             <span aria-current="page">
                                                 <span
-                                                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-500 bg-gray-100 border border-blue-300  cursor-default leading-5">{{ $page }}</span>
+                                                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-blue-500 bg-gray-100 border border-blue-300 z-10 cursor-default leading-5">{{ $page }}</span>
                                             </span>
                                         @else
                                             <button
