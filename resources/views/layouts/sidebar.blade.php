059d74fe9d31a1ed50f4aa558fad830315755fb0
Miftahuddin <53959377+Miftahuddin-cod@users.noreply.github.com>
2022-01-14 07:34:33 +0800

    Set z-index to z-20 on mobile sidebar (#49)

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,7 +17,7 @@
     </div>
 </aside>
 <!-- Mobile sidebar -->
-<aside class="fixed inset-y-0 z-10 flex-shrink-0 w-72 font-poppins mt-16 overflow-y-auto bg-white xl:hidden border-r-2 border-gray-100 shadow-md" x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0 transform -translate-x-20" @click.away="closeSideMenu" @keydown.escape="closeSideMenu" x-cloak>
+<aside class="fixed inset-y-0 z-20 flex-shrink-0 w-72 font-poppins mt-16 overflow-y-auto bg-white xl:hidden border-r-2 border-gray-100 shadow-md" x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0 transform -translate-x-20" @click.away="closeSideMenu" @keydown.escape="closeSideMenu" x-cloak>
     <div class="py-4 text-gray-500">
         <x-logo.full class="ml-5" />
 
