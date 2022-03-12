db15fc6478ca55b5c7bbda088403d604dd910bb1
ekoptra <eko.putrawahyu56@gmail.com>
2022-03-12 20:07:06 +0800

    send email to the konsoler when the asker sends a message

--- app/Http/Livewire/Konsultasi/InputChat.php
+++ app/Http/Livewire/Konsultasi/InputChat.php
@@ -55,36 +55,60 @@ class InputChat extends Component
 
         $this->konsul->update(['updated_at' => now()]);
 
-        $this->sendEmailNotification();
+        // send email notification
+        if ($this->is_admin) $this->sendEmailToAsker();
+        else if ($this->konsul->category == AppKonsul::TYPE_AKADEMIK) $this->sendEmailToAdmin();
 
         $this->emit('reloadComponents', $this->reloadComponent);
         $this->emit('clearChatEditor');
     }
 
-    private function sendEmailNotification()
+    private function sendEmailToAsker()
     {
-        if (!$this->is_admin) return;
-
         $lastChat = $this->konsul->chats()->latest()->skip(1)->first();
+        $lastChatTime = $lastChat ? $lastChat->created_at : $this->konsul->acc_rej_at;
 
-        // kirim email jika chat terakhir bukan dari admin
-        if ($this->is_admin && $lastChat && !$lastChat->is_admin) {
-            $penanya = User::find($this->konsul->user_id);
-            $interval = $penanya->details->setting_send_email_reply_konsultasi ?? 0;
+        $penanya = User::find($this->konsul->user_id);
+        $interval = $penanya->details->setting_send_email_reply_konsultasi ?? 0;
 
-            // kalau settingannya bernilai 0 maka notifikasi email tidak dinyalakan untuk reply konsultasi
-            // jika tidak bernilai 0 maka angka tersebut maksudnya adalah selisih jam dengan last chat dari user
-            if ($interval != 0 && $lastChat->created_at < now()->subHours($interval)) {
-                $message = "Ada pesan baru pada konsultasimu yang berjudul <b>{$this->konsul->title}</b>. Cek sekarang!";
-                $url = route("user.konsultasi.{$this->konsul->category}.room", $this->konsul->id);
+        // kalau settingannya bernilai 0 maka notifikasi email tidak dinyalakan untuk reply konsultasi
+        // jika tidak bernilai 0 maka angka tersebut maksudnya adalah selisih jam dengan last chat dari user
+        if ($interval != 0 && $lastChatTime < now()->subHours($interval)) {
+            $message = "Ada pesan baru pada konsultasimu yang berjudul <b>{$this->konsul->title}</b>. Cek sekarang!";
+            $url = route("user.konsultasi.{$this->konsul->category}.room", $this->konsul->id);
 
-                $penanya->notify(new EmailNotifications((new MailMessage)
+            $penanya->notify(new EmailNotifications((new MailMessage)
+                ->subject("PA60 - Pesan Baru pada Konsultasi")
+                ->greeting("Halo {$penanya->name},")
+                ->line(new HtmlString($message))
+                ->action("Discussion Room", $url)
+                ->line("Regards,")
+                ->salutation("Tim Akademik 60")));
+        }
+    }
+
+    private function sendEmailToAdmin()
+    {
+        $lastChat = $this->konsul->chats()->latest()->skip(1)->first();
+        $lastChatTime = $lastChat ? $lastChat->created_at : $this->konsul->acc_rej_at;
+
+        // send an email if the last chat was 20 minutes ago
+        if ($lastChatTime < now()->subMinutes(20)) {
+            $konselor = AppKonsul::getKonselor(now()->dayOfWeek, $this->konsul->userdetails->jurusan);
+
+            $message = "Terdapat balasan pada konsultasi \"<b>{$this->konsul->title}</b>\" Yuk segera ditanggapi!!";
+
+            $url = route("admin.konsultasi.{$this->konsul->category}.room", $this->konsul->id);
+
+            foreach ($konselor as $nim) {
+                $user  = User::where('email', $nim . '@stis.ac.id')->first();
+                $user->notify(new EmailNotifications((new MailMessage)
                     ->subject("PA60 - Pesan Baru pada Konsultasi")
-                    ->greeting("Halo {$penanya->name},")
+                    ->greeting("Halo {$user->name},")
                     ->line(new HtmlString($message))
                     ->action("Discussion Room", $url)
                     ->line("Regards,")
-                    ->salutation("Tim Akademik 60")));
+                    ->salutation("Tim TI Angkatan 60")));
             }
         }
     }
