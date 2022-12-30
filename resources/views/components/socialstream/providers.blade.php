0088234708fe08bb29c11aed5d3b37f92754a4bd
rii92 <riofebri16@gmail.com>
2022-12-30 11:33:08 +0700

    nonaktifkan fitur login dengan gmail

--- resources/views/components/socialstream/providers.blade.php
+++ resources/views/components/socialstream/providers.blade.php
@@ -1,48 +1,49 @@
-<div class="flex flex-row items-center justify-between py-4 text-gray-500">
+{{-- <div class="flex flex-row items-center justify-between py-4 text-gray-500">
     <hr class="w-full mr-2">
     {{ __('Or') }}
     <hr class="w-full ml-2">
-</div>
+</div> --}}
 
 <div class="flex items-center justify-center">
     @if (JoelButcher\Socialstream\Socialstream::hasFacebookSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::facebook()]) }}">
-            <x-icons.facebook class="h-6 w-6 mx-2" />
+            <x-icons.facebook class="w-6 h-6 mx-2" />
             <span class="sr-only">Facebook</span>
         </a>
     @endif
 
-    @if (JoelButcher\Socialstream\Socialstream::hasGoogleSupport())
+    {{-- nonaktifkan google --}}
+    {{-- @if (JoelButcher\Socialstream\Socialstream::hasGoogleSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::google()]) }}">
-            <x-icons.google class="h-6 w-6 mx-2" />
+            <x-icons.google class="w-6 h-6 mx-2" />
             <span class="sr-only">Google</span>
         </a>
-    @endif
+    @endif --}}
 
     @if (JoelButcher\Socialstream\Socialstream::hasTwitterSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::twitter()]) }}">
-            <x-icons.twitter class="h-6 w-6 mx-2" />
+            <x-icons.twitter class="w-6 h-6 mx-2" />
             <span class="sr-only">Twitter</span>
         </a>
     @endif
 
     @if (JoelButcher\Socialstream\Socialstream::hasLinkedInSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::linkedin()]) }}">
-            <x-icons.linked-in class="h-6 w-6 mx-2" />
+            <x-icons.linked-in class="w-6 h-6 mx-2" />
             <span class="sr-only">LinkedIn</span>
         </a>
     @endif
 
     @if (JoelButcher\Socialstream\Socialstream::hasGithubSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::github()]) }}">
-            <x-icons.github class="h-6 w-6 mx-2" />
+            <x-icons.github class="w-6 h-6 mx-2" />
             <span class="sr-only">GitHub</span>
         </a>
     @endif
 
     @if (JoelButcher\Socialstream\Socialstream::hasGitlabSupport())
         <a href="{{ route('oauth.redirect', ['provider' => JoelButcher\Socialstream\Providers::gitlab()]) }}">
-            <x-icons.gitlab class="h-6 w-6 mx-2" />
+            <x-icons.gitlab class="w-6 h-6 mx-2" />
             <span class="sr-only">GitLab</span>
         </a>
     @endif
@@ -55,7 +56,7 @@
     @endif
 </div>
 
-<div class="italic text-xs text-center mt-4">
+{{-- <div class="mt-4 text-xs italic text-center">
     Use this method for the first time, and select your {{ __('@stis.ac.id') }} email. After that you can set your
     password on profile menu, or stick with this method
-</div>
+</div> --}}
