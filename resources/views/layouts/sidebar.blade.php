2cd57940ab1525f40fd6b4ec22de40db6c2c2f6e
rii92 <riofebri16@gmail.com>
2022-12-31 11:09:56 +0700

    ubah warna background sidebar versi mobile

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -21,7 +21,7 @@
 </aside>
 <!-- Mobile sidebar -->
 <aside
-    class="fixed inset-y-0 z-20 flex-shrink-0 mt-16 overflow-y-auto bg-white border-r-2 border-gray-100 shadow-md w-72 font-poppins xl:hidden"
+    class="fixed inset-y-0 z-20 flex-shrink-0 mt-16 overflow-y-auto border-r-2 border-gray-100 shadow-md bg-blue-sidebar w-72 font-poppins xl:hidden"
     x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150"
     x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100"
     x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100"
