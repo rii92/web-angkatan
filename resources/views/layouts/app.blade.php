2c495f3ee616e5750872daecdff886d622b701ae
Mezuu <alifwirabayu@gmail.com>
2023-02-02 01:41:56 +0700

    fix title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> {{ ucfirst($title) }} | {{ config('app.name', 'Laravel') }}</title>
+    <title> {{ ucfirst($title) }} | {{ config('app.name', 'Web Angkatan 61') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
